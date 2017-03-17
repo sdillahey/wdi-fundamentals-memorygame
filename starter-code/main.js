a44a120ec6ac3508d80ef5f494d64cce1685b0cc
sdillahey <stacy.dillahey@gmail.com>
2017-03-16 17:07:18 -0700

    updated JS, working final draft

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,12 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
+/*var cardOne = "queen";
 
 var cardTwo = "king";
 
 var cardThree = "king";
 
-var cardFour = "queen";
+var cardFour = "queen";*/
 
 /*if (cardOne === cardFour) {
 	alert('Wahoo! You got a match!');
@@ -15,13 +15,42 @@ var cardFour = "queen";
 } else {alert('Sorry, try again.')
 */
 
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
 var gameBoard = document.getElementById('game-board');
 
 var createCards = function (){
-for (var i=0; i<4; i++){
+for (var i=0; i<cards.length; i++){
 	var newCard = document.createElement('div');
 	newCard.className = 'card';
+	newCard.setAttribute('data-card',cards[i]);
+	newCard.addEventListener('click',isTwoCards);
 	gameBoard.appendChild(newCard);
 }}
+
+
+
+var isTwoCards = function (){
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+		if (this.getAttribute('data-card')==='queen') {
+			this.innerHTML = "<img src ='queen.png' alt = 'Queen' style='width:60px;height:60px'>";
+}			else {this.innerHTML = "<img src ='king.jpg' alt = 'King' style='width:60px;height:60px'>";
+		}
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+var isMatch = function (cardsInPlay) {
+ if (cardsInPlay[0]===cardsInPlay[1]){
+ 	alert('Wahoo! You got a match!');
+ } else alert('Sorry, try again!');
+ 	this.innerHTML = " ";
+ 	cardsInPlay = [];
+}
 
 createCards ();
